0d5b5a38139aa4dd48d703cd6bf3727000d343bd
Daron Becerra <daronbc0@gmail.com>
2026-09-21 15:30:35 -0500

    usar puerto dinamico

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ app.get('/api/destinos', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Servidor corriendo en el puerto 3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log('Servidor corriendo en el puerto ' + PORT);
 });
