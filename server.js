f93af69bbed30d48518147cd384ac0d24ac8f87c
Daron Becerra <daronbc0@gmail.com>
2026-09-21 15:52:13 -0500

    escuchar en IPv6

--- server.js
+++ server.js
@@ -20,6 +20,6 @@ app.get('/api/destinos', (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '::', () => {
   console.log('Servidor corriendo en el puerto ' + PORT);
 });
